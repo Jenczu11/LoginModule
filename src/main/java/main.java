522d247939b75d1d74f2031dace19054547b8975
Bartłomiej Jencz <bartek.jencz@wp.pl>
2019-10-14 15:13:17 +0200

    Add files via upload

---
+++ src/main/java/main.java
@@ -0,0 +1,16 @@
+import login.loginManager;
+import login.loginAccess;
+//import login.User;
+public class main {
+    public static void main(String[] args) {
+//        loginManager lm = loginManager.getInstance();
+//        lm.login("admin","admin");
+        loginAccess la = new loginAccess();
+        System.out.println(la.getAccessLevel());
+//        lm.logout();
+        System.out.println(la.getAccessLevel());
+//        lm.login("user","user");
+        System.out.println(la.getAccessLevel());
+
+    }
+}
---
+++ src/main/java/program.java
@@ -0,0 +1,7 @@
+import login.loginManager;
+
+public class program {
+    public static void main(String[] args) {
+        loginManager lm = loginManager.getInstance();
+    }
+}
